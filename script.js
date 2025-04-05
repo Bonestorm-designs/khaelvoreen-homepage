927de0ca2d35a67eab3f27e069afa8c4ef186307
Bonestorm-designs <jccastillo1985@gmail.com>
2025-04-05 14:10:25 +1100

    Update script.js

--- script.js
+++ script.js
@@ -7,26 +7,18 @@ const starGlow = document.getElementById('toggle-glow-star'); // optional new co
 let isOpen = false;
 
 function updatePositions() {
-    const sidebar = document.getElementById('sidebar');
-    const toggleArrow = document.getElementById('sidebar-toggle');
-    const glow = document.getElementById('glow-outline');
-    const burst = document.getElementById('glow-behind-arrow');
-
     if (isOpen) {
         sidebar.style.left = "0";
         toggleArrow.style.left = "23.5vw";
-        glow.style.left = "26.5vw";
-        burst.style.left = "26.5vw";
-        toggleArrow.style.backgroundImage = "url(openside.png)";
+        document.getElementById('toggle-glow-star').style.left = "26.5vw";
+        toggleArrow.style.backgroundImage = "url('openside.png')";
     } else {
         sidebar.style.left = "-20vw";
         toggleArrow.style.left = "3.5vw";
-        glow.style.left = "5vw";
-        burst.style.left = "5vw";
-        toggleArrow.style.backgroundImage = "url(closedside.png)";
+        document.getElementById('toggle-glow-star').style.left = "5vw";
+        toggleArrow.style.backgroundImage = "url('closedside.png')";
     }
 }
-
 
 toggleArrow.addEventListener("click", () => {
   isOpen = !isOpen;
